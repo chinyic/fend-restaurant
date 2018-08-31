16e644c70e864441a4a6fab0f1d2d8124481dfbc
Chinyi <chinyi.cc@gmail.com>
2018-08-31 13:38:28 -0700

    create varcachefiles

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+var cacheName = 'v1';
+
+// Default files to always cache
+var cacheFiles = [
+	'./',
+	'./index.html',
+  './restaurant.html',
+	'./js/main.js',
+  './js/dbhelper.js',
+  './js/restaurant_info.js',
+	'./css/style.css',
+	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic'
+]
+
+
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Installed');
 
