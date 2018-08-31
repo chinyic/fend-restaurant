00cf96a820537feb6aa8e023b4bf87d33ea7abe3
Chinyi <chinyi.cc@gmail.com>
2018-08-31 13:58:15 -0700

    fixed cache files

--- sw.js
+++ sw.js
@@ -8,14 +8,23 @@ var cacheFiles = [
 	'./js/main.js',
   './js/dbhelper.js',
   './js/restaurant_info.js',
-	'./css/style.css',
+	'./css/styles.css',
 	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic'
 ]
 
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Installed');
+//wait until promise is resolved
+    e.waitUntil(
+      // Open the cache
+	    caches.open(cacheName).then(function(cache) {
 
+	    	// Add all the default files to the cache
+			console.log('[ServiceWorker] Caching cacheFiles');
+			return cache.addAll(cacheFiles);
+	    })
+	); // end e.waitUntil
 });
 
 self.addEventListener('activate', function(e) {
