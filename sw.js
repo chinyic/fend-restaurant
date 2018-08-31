3aec456da15ba1414b779024fd8d8d95fc1cc678
Chinyi <chinyi.cc@gmail.com>
2018-08-31 13:32:07 -0700

    updated sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed');
+
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activated');
+
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetching');
+
+});
