8b536c5d47b0da2921837129f5ae5626b056626d
Chin Yi Chen <chinyic@uoregon.edu>
2018-09-02 13:02:08 -0700

    added image.alt

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-image.setAttribute("alt", `An image of the restaurant.`);
+//image.setAttribute("alt", `An image of the restaurant.`);
+  image.alt = "Image of" + restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
